f8deeb4e3eee91baac1f7108b8974ffe8694b0c2
Evgeniy Zhurichev <zeraxas@gmail.com>
2018-08-21 18:23:05 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+let options = {
+    strings: ["Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.",
+      "Very little is needed to make a happy life; it is all within yourself, in your way of thinking.",
+      "You have power over your mind - not outside events. Realize this, and you will find strength.",
+      "The universe is change; our life is what our thoughts make it.",
+      "Death smiles at us all, but all a man can do is smile back.",
+      "Waste no more time arguing about what a good man should be. Be one.",
+      "The soul becomes dyed with the color of its thoughts.",
+      "A man's worth is no greater than his ambitions."],
+    typeSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+    fadeOut: true,
+    backDelay: 1700,
+    shuffle: true,
+    showCursor: false
+  }
+
+  let typed = new Typed(".primary-quote", options);
